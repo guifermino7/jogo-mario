204abeca7f0cf9545ca2409cfd9915abb89697b2
guifermino7 <guigamf@hotmail.com>
2023-06-22 00:07:12 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+const jump = () => {
+    const mario = document.querySelector('.mario');
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
+
+const loop = setInterval(() => {
+    const pipe = document.querySelector('.pipe');
+    const mario = document.querySelector('.mario');
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+
+    console.log(marioPosition);
+    
+    if (pipePosition <= 105 && pipePosition > 0 && marioPosition < 80) {
+        pipe.style.animation = 'none';
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPosition}px`;
+
+        mario.src = './assets/img/game-over.png';
+        mario.style.width = '3.6rem';
+        mario.style.rotate = '-3.5deg';
+        mario.style.marginLeft = '3rem';
+
+        clearInterval(loop);
+    }
+    
+}, 10);
+
+document.addEventListener('keydown', jump);
