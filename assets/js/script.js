5ba47280b29f180a87ddd1b4c7812a57013b8a0e
guifermino7 <guigamf@hotmail.com>
2023-06-28 00:28:10 -0300

    Adicionando melhorias

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,25 +8,31 @@ const jump = () => {
 }
 
 const loop = setInterval(() => {
+
     const koopa = document.querySelector('.koopa');
     const mario = document.querySelector('.mario');
+    const nuvens = document.querySelector('.clouds');
     
     const koopaPosition = koopa.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
-
-    console.log(marioPosition);
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+    const nuvensPosition = nuvens.offsetLeft;
     
-    if (koopaPosition <= -10 && marioPosition < 40) {
+    if (koopaPosition <= 95 && koopaPosition > 0 && marioPosition < 220) {
         koopa.style.animation = 'none';
         koopa.style.left = `${koopaPosition}px`;
 
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
+        nuvens.style.animation = 'none';
+        nuvens.style.left = `${nuvensPosition}px`;
+
         mario.src = './assets/img/game-over.png';
         mario.style.width = '3.6rem';
         mario.style.rotate = '-3.5deg';
         mario.style.marginLeft = '3rem';
+
+        koopa.src = './assets/img/koopa.png'
 
         clearInterval(loop);
     }
